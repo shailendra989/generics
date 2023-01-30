f2f22469787bc811e21e9a7a30b4060e9c408a04
Shailendra kumar pal <palshailendrakumar123@gmail.com>
2023-01-30 22:43:10 +0530

    file push

---
+++ PrintArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenricsDemo
+{
+    internal class PrintArraY
+    {
+        class PrintArray<T>
+        {
+            public static void toPrint<T>(T[] inputArray)
+            {
+                foreach (T element in inputArray)
+                {
+                    Console.WriteLine(element);
+                }
+                Console.WriteLine("----------------------------------");
+            }
+
+            //public static void toPrint(int[] inputArray)
+            //{
+            //    foreach (var element in inputArray)
+            //    {
+            //        Console.WriteLine(element);
+            //    }
+            //    Console.WriteLine("----------------------------------");
+            //}
+            //public static void toPrint(double[] inputArray)
+            //{
+            //    foreach (var element in inputArray)// 1,5,3,8,9
+            //    {
+            //        Console.WriteLine(element);
+            //    }
+            //    Console.WriteLine("----------------------------------");
+            //}
+
+            //public static void toPrint(char[] inputArray)
+            //{
+            //    foreach (var element in inputArray)
+            //    {
+            //        Console.WriteLine(element);
+            //    }
+            //    Console.WriteLine("-------------------------------------");
+            //}
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace GenricsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+            bool[] boolArr = { true, false };
+            //GenericsMethod.toPrint(intArray);
+            //GenericsMethod.toPrint(doubleArray);
+            //GenericsMethod.toPrint(charArray);
+
+            //GenericsMethod.toPrint<int>(intArray);
+            //GenericsMethod.toPrint<double>(doubleArray);
+            //GenericsMethod.toPrint<char>(charArray);
+            //GenericsMethod.toPrint<bool>(boolArr);
+            //PrintArray<int> obj = new PrintArray<int>(intArray);
+            //obj.toPrint();
+
+
+
+        }
+    }
+}
